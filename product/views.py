a08d500ab574d2f5168334f7199b318833077464
Nonashi37 <argeno37@gmail.com>
2026-05-08 22:33:43 +0600

    added PUT delete and e.t.c also validations

--- product/views.py
+++ product/views.py
@@ -14,59 +14,127 @@ from .serializers import (
 
 # --- CATEGORY ---
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def category_list_api_view(request):
-    # Let the DB count the products for us
-    categories = Category.objects.annotate(products_count=Count('product'))
-    serializer = CategorySerializer(categories, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        # Changed 'product' to 'products' to match your model's related_name
+        categories = Category.objects.annotate(products_count=Count('products'))
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def category_detail_api_view(request, id):
-    # Detail views usually don't need the count, but you can add it if needed
     category = get_object_or_404(Category, id=id)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+
+    if request.method == 'GET':
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = CategorySerializer(instance=category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 # --- PRODUCT ---
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def product_list_api_view(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # Ensured this return is outside the if block and has the full status name
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET'])
 def product_reviews_list_api_view(request):
     """
-    New endpoint: /api/v1/products/reviews/
-    Returns products with their reviews and average stars.
+    Returns products with their nested reviews and average stars.
     """
-    # .prefetch_related('reviews') prevents the "N+1" query problem!
-    products = Product.objects.prefetch_related('reviews').annotate(
-        average_rating=Avg('reviews__stars')
-    )
-    serializer = ProductReviewSerializer(products, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        # prefetch_related fixes the N+1 query problem!
+        products = Product.objects.prefetch_related('reviews').annotate(
+            average_rating=Avg('reviews__stars')
+        )
+        serializer = ProductReviewSerializer(products, many=True)
+        return Response(serializer.data)
+    
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail_api_view(request, id):
     product = get_object_or_404(Product, id=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+
+    if request.method == 'GET':
+        serializer = ProductReviewSerializer(product)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(instance=product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 # --- REVIEW ---
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def review_list_api_view(request):
-    reviews = Review.objects.all()
-    serializer = ReviewSerializer(reviews, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        reviews = Review.objects.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = ReviewSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def review_detail_api_view(request, id):
+    #Moved to top so 'review' is available for PUT and DELETE
     review = get_object_or_404(Review, id=id)
-    serializer = ReviewSerializer(review)
-    return Response(serializer.data)
+    
+    if request.method == 'GET':
+        serializer = ReviewSerializer(review)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = ReviewSerializer(instance=review, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        review.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
