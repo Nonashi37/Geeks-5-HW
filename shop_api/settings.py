69d482de190d9441a4ddf8c2bb851e9ae79d1fb1
Nonashi37 <argeno37@gmail.com>
2026-05-18 10:18:31 +0600

    authorizations

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third-party apps
     'rest_framework',
+    'rest_framework.authtoken',
+
+    # your apps
     'product',
 
 
